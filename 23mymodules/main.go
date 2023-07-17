fcf6d68f2b6123b356984de9ae47874633ea6b23
shoebilyas123 <shoebilyas123@gmail.com>
2023-07-17 14:54:36 +0530

    go mod and mux

--- 23mymodules/main.go
+++ 23mymodules/main.go
@@ -1,8 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 
 func main() {
 	fmt.Println("Hello MOD")
+	r := mux.NewRouter();
+	r.HandleFunc("/home", serveHome).Methods("GET")
+	
+	log.Fatal(http.ListenAndServe("http://localhost:8000", r))
+}
+
+
+func serveHome(res http.ResponseWriter, req *http.Request){
+	res.Write([]byte("<h1>Welcome to Golang Learningsss....</h1>"))
 }
